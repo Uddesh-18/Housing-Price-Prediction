1a1d1138ec1c509a4c35d804eb1e813840104085
Uddesh-18 <uddesh543410@gmail.com>
2024-09-21 16:18:03 +0530

    Add files via upload

---
+++ templates/Home.js
@@ -0,0 +1,25 @@
+function form_handler(event)
+{
+       event.preventDefault();
+}
+function send_data()
+{
+        document.querySelector('form').addEventListener("submit",form_handler);
+
+        var fd=new FormData(document.querySelector('form'));
+
+        var xhr= new XMLHttpRequest();
+
+        xhr.open('POST','/predict',true);
+
+        document.getElementById("prediction").innerHTML = "Wait Predicting Price...!";
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == XMLHttpRequest.DONE){
+                document.getElementById('prediction').innerHTML="Estimated Price is : ₹ "+xhr.responseText;
+            }
+        };
+
+        xhr.onload = function(){};
+        xhr.send(fd);
+
+}
